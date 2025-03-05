adeb557d9ca14b7029c6620b145dc9c194e4c783
TravisDickens <Travis.Dickens@outlook.com>
2025-03-05 18:44:04 +0200

    first commit

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Transacation> Transacations { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
